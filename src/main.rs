5e9d47291e9eb5ccb64c9b3ce2d066abc9f274d1
Ongsalt <ongsa.srp.mkm@gmail.com>
2025-09-23 09:00:59 +0700

    triangle

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-use gl::Viewport;
+use core::str;
+use std::{
+    ffi::{CString, c_void},
+    ptr,
+};
+
+use gl::{
+    Viewport,
+    types::{GLchar, GLint, GLsizeiptr},
+};
 use glfw::{Action, Context, GlfwReceiver, Key, OpenGlProfileHint, WindowHint};
 
-const WIDTH: u64 = 800;
-const HEIGHT: u64 = 800;
+// https://learnopengl.com/Getting-started/Hello-Triangle
+
+const VERTEX_SHADER_SOURCE: &str = include_str!("../shaders/1.vert");
+const FRAGMENT_SHADER_SOURCE: &str = include_str!("../shaders/1.frag");
 
 fn main() {
-    println!("Hello, world!");
-
     let mut glfw = glfw::init(glfw::fail_on_errors).expect("unable to initialize glfw");
 
     glfw.window_hint(WindowHint::ContextVersion(3, 3));
@@ -24,16 +33,89 @@ fn main() {
 
     gl::load_with(|symbol| window.get_proc_address(symbol).unwrap() as *const _);
 
-    unsafe {
-        Viewport(0, 0, WIDTH as i32, HEIGHT as i32);
-    }
+    let shader_program = unsafe {
+        let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
+        let c_string = CString::new(VERTEX_SHADER_SOURCE.as_bytes()).unwrap();
+        gl::ShaderSource(vertex_shader, 1, &c_string.as_ptr(), ptr::null());
+        gl::CompileShader(vertex_shader);
+
+        let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+        let c_string = CString::new(FRAGMENT_SHADER_SOURCE.as_bytes()).unwrap();
+        gl::ShaderSource(fragment_shader, 1, &c_string.as_ptr(), ptr::null());
+        gl::CompileShader(fragment_shader);
+
+        let shader_program = gl::CreateProgram();
+        gl::AttachShader(shader_program, vertex_shader);
+        gl::AttachShader(shader_program, fragment_shader);
+        gl::LinkProgram(shader_program);
+
+        // let mut info_log = Vec::with_capacity(512);
+        // gl::GetProgramInfoLog(
+        //     shader_program,
+        //     512,
+        //     ptr::null_mut(),
+        //     info_log.as_mut_ptr() as *mut GLchar,
+        // );
+        // println!("ProgramInfoLog\n{}", str::from_utf8(&info_log).unwrap());
+
+        gl::DeleteShader(fragment_shader);
+        gl::DeleteShader(vertex_shader);
+
+        // let mut success: GLint = 0;
+        // gl::GetShaderiv(vertex_shader, gl::COMPILE_STATUS, &mut success);
+        // println!("success = {success}");
+        shader_program
+    };
+
+    let (vao, vbo) = unsafe {
+        #[rustfmt::skip]
+        let vertices: [f32; 9] = [
+            0.0,  0.5, 0.0,
+            0.5, -0.5, 0.0,
+            -0.5, -0.5, 0.0,
+        ];
+
+        let mut vbo = 0;
+        let mut vao = 0;
+
+        gl::GenBuffers(1, &mut vbo);
+        // println!("vbo: {vbo}");
+
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (vertices.len() * size_of::<f32>()) as _,
+            &vertices[0] as *const _ as _, // fuck
+            gl::STATIC_DRAW,
+        );
+
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            3 * size_of::<f32>() as i32,
+            // &0 as *const _ as _,
+            ptr::null()
+        );
+
+        gl::EnableVertexAttribArray(0);
+        (vao, vbo)
+    };
 
     while !window.should_close() {
         process_events(&mut window, &events);
 
         unsafe {
-            gl::ClearColor(1.0, 0.0, 0.0, 0.0);
+            gl::ClearColor(0.4, 0.4, 0.4, 0.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
+        
+            gl::UseProgram(shader_program);
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
         }
 
         window.swap_buffers();
