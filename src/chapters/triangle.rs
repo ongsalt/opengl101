4e235fa41b9c09587da318d84d292e29ba453975
Ongsalt <ongsa.srp.mkm@gmail.com>
2025-12-07 23:40:59 +0700

    use modern api

--- src/chapters/triangle.rs
+++ src/chapters/triangle.rs
@@ -6,13 +6,10 @@ use crate::{glfw::process_events, shader::Shader, shared::get_string};
 
 // https://learnopengl.com/Getting-started/Hello-Triangle
 
-// const VERTEX_SHADER_SOURCE: &str = include_str!("../../shaders/1.vert");
-// const FRAGMENT_SHADER_SOURCE: &str = include_str!("../../shaders/1.frag");
-
 pub fn run() {
     let mut glfw = glfw::init(glfw::fail_on_errors).expect("unable to initialize glfw");
 
-    glfw.window_hint(WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(WindowHint::ContextVersion(4, 6));
     glfw.window_hint(WindowHint::OpenGlProfile(OpenGlProfileHint::Core));
 
     let (mut window, events) = glfw
@@ -25,71 +22,58 @@ pub fn run() {
 
     gl::load_with(|symbol| window.get_proc_address(symbol).unwrap() as *const _);
 
-    println!("OpenGL version: {}", glfw::get_version_string());
+    println!("OpenGL version: {}", get_string(gl::VERSION).unwrap());
     println!("Renderer: {}", get_string(gl::RENDERER).unwrap());
 
     let shader = Shader::from_files("./shaders/1.vert", "./shaders/1.frag");
 
-    let (vao, _, _) = unsafe {
+    let (vao, _, ebo) = unsafe {
         #[rustfmt::skip]
-        let vertices: [f32; 18] = [
+        let vertices: [f32; 24] = [
              0.0,  0.5,  0.0,  1.0,  0.0,  0.0,
              0.5, -0.5,  0.0,  0.0,  1.0,  0.0,
             -0.5, -0.5,  0.0,  0.0,  0.0,  1.0,
+             0.0, -1.0,  0.0,  1.0,  1.0,  1.0,
         ];
 
-        // let indices: [u32; 6] = [0, 1, 2, 1, 2, 3];
+        let indices: [u32; 6] = [0, 1, 2, 1, 2, 3];
 
         let mut vbo = 0;
-        gl::GenBuffers(1, &mut vbo);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-
-        let mut vao = 0;
-        gl::GenVertexArrays(1, &mut vao);
-        gl::BindVertexArray(vao);
-
-        let mut ebo = 0;
-        gl::GenBuffers(1, &mut ebo);
-        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
-
-        // gl::BufferData(
-        //     gl::ELEMENT_ARRAY_BUFFER,
-        //     (indices.len() * size_of::<u32>()) as _,
-        //     &indices[0] as *const _ as _, // fuck
-        //     gl::STATIC_DRAW,
-        // );
-
-        gl::BufferData(
-            gl::ARRAY_BUFFER,
+        gl::CreateBuffers(1, &mut vbo);
+        gl::NamedBufferData(
+            vbo,
             (vertices.len() * size_of::<f32>()) as _,
             &vertices[0] as *const _ as _, // fuck
             gl::STATIC_DRAW,
         );
 
-        // position
-        gl::VertexAttribPointer(
-            0,
-            3,
-            gl::FLOAT,
-            gl::FALSE,
-            6 * size_of::<f32>() as i32,
-            // &0 as *const _ as _,
-            ptr::null(),
+        let mut vao = 0;
+        gl::CreateVertexArrays(1, &mut vao);
+
+        let mut ebo = 0;
+        gl::CreateBuffers(1, &mut ebo);
+        gl::NamedBufferData(
+            ebo,
+            (indices.len() * size_of::<u32>()) as _,
+            &indices[0] as *const _ as _, // fuck
+            gl::STATIC_DRAW,
         );
-        gl::EnableVertexAttribArray(0);
+
+        gl::VertexArrayElementBuffer(vao, ebo);
+
+        // position
+        gl::VertexArrayAttribFormat(vao, 0, 3, gl::FLOAT, gl::FALSE, 0);
+        gl::VertexArrayAttribBinding(vao, 0, 0);
+        gl::EnableVertexArrayAttrib(vao, 0);
 
         // color
-        gl::VertexAttribPointer(
-            1,
-            3,
-            gl::FLOAT,
-            gl::FALSE,
-            6 * size_of::<f32>() as i32,
-            (3 * size_of::<f32>()) as *const _,
-        );
-        gl::EnableVertexAttribArray(1);
+        gl::VertexArrayAttribFormat(vao, 1, 3, gl::FLOAT, gl::FALSE, 3 * size_of::<f32>() as u32);
+        gl::VertexArrayAttribBinding(vao, 1, 0);
+        gl::EnableVertexArrayAttrib(vao, 1);
 
         // gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
+
+        gl::VertexArrayVertexBuffer(vao, 0, vbo, 0, (6 * size_of::<f32>()) as _);
 
         (vao, vbo, ebo)
     };
@@ -101,13 +85,12 @@ pub fn run() {
             gl::ClearColor(1.0, 1.0, 1.0, 0.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
 
-            // gl::UseProgram(shader_program);
-            shader.use_program();
             gl::BindVertexArray(vao);
+            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
 
-            // gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
-            // gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
+            shader.use_program();
+
+            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
         }
 
         window.swap_buffers();
